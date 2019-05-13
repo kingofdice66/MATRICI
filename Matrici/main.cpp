3a0eecfdff37e3e807c2da1cec56dcf80c2a6c63
kingofdice66 <50295155+kingofdice66@users.noreply.github.com>
2019-05-13 18:41:58 +0300

    mici modificari verificare date intrare

--- Matrici/main.cpp
+++ Matrici/main.cpp
@@ -17,13 +17,18 @@ int main()
 
 		std::cout << "Introduceti numarul de ecuatii si necunoscute: ";
 		std::cin >> n;
-		while (std::cin.fail())
+
+		while ((n <= 0) || (std::cin.fail()))
 		{
+			std::cout << "\n\n";
+			std::cout << "Numarul introdus trebuie sa fie mai mare de zero(0)!\n";
+			
 			std::cin.clear();
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			std::cout << "Se accepta numai numere!\n";
 			std::cout << "Introduceti numarul de ecuatii si necunoscute: ";
 			std::cin >> n;
+			
 		}
 
 		while (std::cin.get() != '\n') // de exemplu daca se introduce n = 3aaa, cei trei de 'a' se ignora pana la \n si ramane numai 3
